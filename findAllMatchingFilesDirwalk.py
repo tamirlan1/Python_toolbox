1311c27df5dac8c745dc2ce73327b94ebba7ad65
Tamirlan Seidakhmetov <tamirlan@tamirlans-mbp.win.cccis.com>
2017-08-17 13:46:09 -0500

    Add findAllMatchingFilesDirwalk.py script

---
+++ findAllMatchingFilesDirwalk.py
@@ -0,0 +1,17 @@
+'''
+Function visits every folder in the specified directory and finds all files matching the search
+'''
+
+import os
+import fnmatch
+
+def getListOfFilePaths(path, search_criteria):
+	matches = []
+	for root, dirnames, filenames in os.walk(path):
+	    for filename in fnmatch.filter(filenames, search_criteria):
+	        matches.append(os.path.join(root, filename))
+	return matches
+
+path = "/Users/tamirlan/Documents/Personal/iPhoto Library.migratedphotolibrary/Masters"
+
+print len(getListOfFilePaths(path, '*.jpg'))
