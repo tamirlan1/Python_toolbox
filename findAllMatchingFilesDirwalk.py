7f6845552357c624b9f285d3c89d82a056376eca
Tamirlan Seidakhmetov <tamirlan@Tamirlans-MacBook-Pro.local>
2018-02-08 13:26:27 -0600

    modified tabs vs spaces

--- findAllMatchingFilesDirwalk.py
+++ findAllMatchingFilesDirwalk.py
@@ -6,12 +6,12 @@ import os
 import fnmatch
 
 def getListOfFilePaths(path, search_criteria):
-	matches = []
-	for root, dirnames, filenames in os.walk(path):
-	    for filename in fnmatch.filter(filenames, search_criteria):
-	        matches.append(os.path.join(root, filename))
-	return matches
+    matches = []
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, search_criteria):
+            matches.append(os.path.join(root, filename))
+    return matches
 
-path = "/Users/tamirlan/Documents/Personal/iPhoto Library.migratedphotolibrary/Masters"
-
+# path = "/Users/tamirlan/Documents/Personal/iPhoto Library.migratedphotolibrary/Masters"
+path = "/Users/tamirlan/Documents/Personal/iPhoto Library.migratedphotolibrary/Previews"
 print len(getListOfFilePaths(path, '*.jpg'))
